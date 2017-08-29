9a29563bf787573aab29bba0c70a68ca54cc9a3d
alcuervo <alcuervo@personalsoft.com>
2017-08-29 11:26:41 -0500

    avance en el código de la calculadora

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
 // alejandro cuervo
 
-var Calculadora={
+//variables
+var display = null;
+var resultado = 0;
+var memoria1 = '0';
+
+//objeto
+var Calculadora = {
   init:function(){
-    this.funciones();
     this.iniciarListeners();
+    display=document.getElementById('display');
   },
 
   iniciarListeners:function(){
+    var calc=this;
     var botonesNumericos=[];
     for(var i=0;i<=9;i++){
       botonesNumericos.push(document.getElementById(i));
       botonesNumericos[i].addEventListener('click',function(){
-        agregarNumero(i);
+        calc.agregarNumero(this);
       });
     }
 
-    document.getElementById('on').addEventListener('click',this.funciones.encender);
+    document.getElementById('on').addEventListener('click',this.encender);
     document.getElementById('sign').addEventListener('click',this.sign);
     document.getElementById('por').addEventListener('click',this.multiplicar);
     document.getElementById('menos').addEventListener('click',this.restar);
@@ -24,27 +31,46 @@ var Calculadora={
     document.getElementById('igual').addEventListener('click',this.igual);
   },
 
-  funciones: function(){
-    function encender(){
+  agregarNumero: function(btn){
+    var num=btn.id;
+    if(memoria1==0){
+      if(num!=0){
+        memoria1=num;
+      }
+    }else {
+      memoria1=memoria1+num;
+    }
 
-    };
-    function sign(){
+    this.imprimirNumero();
+  },
+  encender: function(){
+    display.innerHTML = '0';
+    memoria1=0;
+  },
+  sign:function(){
 
-    };
-    function multiplicar(){
+  },
+  multiplicar:function(){
 
-    };
-    function restar(){
+  },
+  restar:function(){
 
-    };
-    function sumar(){
+  },
+  sumar:function(){
 
-    };
-    function punto(){
+  },
+  punto:function(){
 
-    };
-    function igual(){
+  },
+  igual:function(){
 
+  },
+  imprimirNumero(){
+    if(memoria1.length>=8){
+      var memStr=memoria1+"";
+      display.innerHTML = memStr.substring(0,8);
+    }else{
+      display.innerHTML = memoria1;
     }
   }
 };

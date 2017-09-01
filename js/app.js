c0d06df78c0ae519b1ed690c284401ad6c140d36
alcuervo <alcuervo@personalsoft.com>
2017-09-01 08:16:36 -0500

    Calculadora terminada en fase beta

--- js/app.js
+++ js/app.js
@@ -1,78 +1,144 @@
-// alejandro cuervo
+// Alejandro Cuervo G
 
-//variables
-var display = null;
-var resultado = 0;
-var memoria1 = '0';
+var Calculadora=(function(){
+  var display = null;
+  var resultado = 0;
+  var memoria1 = '0';
+  var memoria2 = '0';
+  var operacion = '';
 
-//objeto
-var Calculadora = {
-  init:function(){
-    this.iniciarListeners();
-    display=document.getElementById('display');
-  },
-
-  iniciarListeners:function(){
-    var calc=this;
-    var botonesNumericos=[];
-    for(var i=0;i<=9;i++){
-      botonesNumericos.push(document.getElementById(i));
-      botonesNumericos[i].addEventListener('click',function(){
-        calc.agregarNumero(this);
-      });
-    }
-
-    document.getElementById('on').addEventListener('click',this.encender);
-    document.getElementById('sign').addEventListener('click',this.sign);
-    document.getElementById('por').addEventListener('click',this.multiplicar);
-    document.getElementById('menos').addEventListener('click',this.restar);
-    document.getElementById('mas').addEventListener('click',this.sumar);
-    document.getElementById('punto').addEventListener('click',this.punto);
-    document.getElementById('igual').addEventListener('click',this.igual);
-  },
-
-  agregarNumero: function(btn){
-    var num=btn.id;
-    if(memoria1==0){
-      if(num!=0){
-        memoria1=num;
-      }
-    }else {
-      memoria1=memoria1+num;
-    }
-
-    this.imprimirNumero();
-  },
-  encender: function(){
+  var encender = function(){
     display.innerHTML = '0';
-    memoria1=0;
-  },
-  sign:function(){
+    memoria1='0';
+    memoria2='0';
+  }
 
-  },
-  multiplicar:function(){
+  var sign = function(){
+    memoria1=String(memoria1*-1);
+    imprimirPantalla('');
+  }
+  var dividir = function(){
+    operacion='dividir';
+    preOperar2();
+  }
+  var multiplicar = function(){
+    operacion='multiplicar';
+    preOperar2();
+  }
+  var restar = function(){
+    operacion='restar';
+    preOperar2();
+  }
+  var sumar = function(){
+    operacion='sumar';
+    if(memoria2=='0'){
+      preOperar();
+    }else{
+      memoria2 = operar(memoria2,memoria1,operacion);
+      memoria1='0';
+      display.innerHTML='0';
+    }
+  }
 
-  },
-  restar:function(){
+  function preOperar(){
+    memoria2=memoria1;
+    memoria1='0';
+    display.innerHTML='0';
+  }
 
-  },
-  sumar:function(){
+  function preOperar2(){
+    if(memoria2=='0'){
+      memoria2=memoria1;
+      memoria1='0';
+      display.innerHTML='0';
+    }else{
+      memoria2 = operar(memoria2,memoria1,operacion);
+      memoria1='0';
+      display.innerHTML='0';
+    }
+  }
 
-  },
-  punto:function(){
+  function operar(var1,var2,oper){
+    switch (oper) {
+      case 'dividir':
+        return String(parseFloat(var2)/parseFloat(var1));
+      break;
+      case 'multiplicar':
+        return String(parseFloat(var2)*parseFloat(var1));
+      break;
+      case 'restar':
+        return String(parseFloat(var2)-parseFloat(var1));
+      break;
+      case 'sumar':
+        return String(parseFloat(var2)+parseFloat(var1));
+        break;
+      default:
+        return;
+    }
+  }
 
-  },
-  igual:function(){
+  var punto = function(){
+    var punto=memoria1.indexOf('.',0);
 
-  },
-  imprimirNumero(){
+    if(punto==-1 && memoria1!='0'){
+      imprimirPantalla('.');
+    }
+  }
+
+  var igual = function(){
+    memoria1 = operar(memoria1,memoria2,operacion);
+
     if(memoria1.length>=8){
-      var memStr=memoria1+"";
-      display.innerHTML = memStr.substring(0,8);
+      display.innerHTML = memoria1.substring(0,8);
+    }else{
+      display.innerHTML = memoria1;
+    }
+    memoria1 = '0'; memoria2 = '0';
+  }
+
+  var imprimirPantalla = function(caracter){
+    if(memoria1=='0'){
+      if(caracter!=0){
+        memoria1=String(caracter);
+      }
+    }else{
+      memoria1=memoria1+String(caracter);
+    }
+    //imprimirPantalla
+    if(memoria1.length>=8){
+      display.innerHTML = memoria1.substring(0,8);
     }else{
       display.innerHTML = memoria1;
     }
   }
-};
+
+  function iniciarListeners(){
+    var botonesNumericos=[];
+    for(var i=0;i<=9;i++){
+      (function(posActual){
+        botonesNumericos.push(document.getElementById(i));
+        botonesNumericos[i].addEventListener('click',function(){
+          imprimirPantalla(posActual);
+        });
+      })(i);
+    }
+    document.getElementById('on').addEventListener('click',encender);
+    document.getElementById('sign').addEventListener('click',sign);
+    document.getElementById('dividido').addEventListener('click',dividir);
+    document.getElementById('por').addEventListener('click',multiplicar);
+    document.getElementById('menos').addEventListener('click',restar);
+    document.getElementById('mas').addEventListener('click',sumar);
+    document.getElementById('punto').addEventListener('click',punto);
+    document.getElementById('igual').addEventListener('click',igual);
+  }
+
+  return{
+    init: function(){
+      iniciarListeners();
+      display=document.getElementById('display');
+    }
+  }
+
+})();
 
 Calculadora.init();
